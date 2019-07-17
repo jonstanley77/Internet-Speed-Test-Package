db4f33c2ba83d8b19962f540576339617e8c0678
jonstanley77 <jonstanley1999@gmail.com>
2019-07-17 16:14:20 -0400

    nmap script update

--- EmailNotifications/emailnotification.sh
+++ EmailNotifications/emailnotification.sh
@@ -1,25 +1,84 @@
 #!/bin/sh /etc/rc.common
 # Email notification boot script
-
  
 START=99 
 STOP=1
 
 
- 
-start() {   
-		alertChoice="0"
-		runningAlert="Your device is now up and running! "
+boot(){
 
-		echo "Boot notification being sent now..."
+	if [[ -e $email/email.txt ]]
+	then
+		emailboot=$(cat $email/email.txt)
+		echo "email address for this user is: $emailboot"
+	else
+		touch email/email.txt
+		echo "there is no saved email for notifications, what email address would you like to use?: "
+		read varemail
+		echo $varemail > $email/email.txt
+		emailboot=$(cat $email/email.txt)
+		echo "email address saved"
+	fi
+
+	/quick_mount
+	echo "boot notification being sent now..."
+	sleep 10
+	echo "boot notification send confirmed :)"
+
+	python3 /mnt/mmcblk0p3/ubuntu/etc/my_mail/smtprelay.py "Your device is up and running" "$emailboot"
+	sleep 10
+	while [ true ]
+	do
+		nohup /etc/nmapscripts/connected_devices.sh newdevicecheck >/dev/null
+		sleep 30
+	done
+
+}
+
+ 
+start() { 
+
+		if [[ -e $email/email.txt ]]
+		then
+			emailstart=$(cat $email/email.txt)
+			echo "email address for this user is: $emailstart"
+		else
+			touch email/email.txt
+			echo "there is no saved email for notifications, what email address would you like to use?: "
+			read varemail
+			echo $varemail > $email/email.txt
+			emailstart=$(cat $email/email.txt)
+			echo "email address saved"
+		fi
+
+		echo "network notification being sent now..."
 		sleep 2
 
-		if [[ $alertChoice -eq 0 ]]
-		then
-			python /email/smtprelay.py "$runningAlert"
+		choice=$1
+		deviceAm=$2
+
+		if [[ $choice -gt 4 ]]; then
+			echo "invalid parameter passed to options field, please investigate"
 		fi
+
+		if [[ $choice -eq 1 ]]; then
+			python3 /mnt/mmcblk0p3/ubuntu/etc/my_mail/smtprelay.py "Your device is now up and running" "$emailstart"
+ 	  	fi
+
+ 	  	if [[ $choice -eq 2 ]]; then
+			python3 /mnt/mmcblk0p3/ubuntu/etc/my_mail/smtprelay.py "You have a suspicious port open!" "$emailstart"
+ 	  	fi
+
+ 	  	if [[ $choice -eq 3 ]]; then
+			python3 /mnt/mmcblk0p3/ubuntu/etc/my_mail/smtprelay.py "There appears to be $2 new device(s) connected to your network!" "$emailstart"
+ 	  	fi
+
+ 	  	if [[ $choice -eq 4 ]]; then
+			python3 /mnt/mmcblk0p3/ubuntu/etc/my_mail/smtprelay.py "Your internet speed is below average" "$emailstart"
+ 	  	fi
+
 }
  
 stop() {          
-        echo "Boot notification has been sent..."
+        echo "network notification has been sent..."
 }
--- NMap/connected_devices.sh
+++ NMap/connected_devices.sh
@@ -1,44 +1,97 @@
 #!/bin/bash
 #script to pull device ips connected to wlan0 and the times they have been up
-rm devicelog.txt
-if [[ -e $devicelog.txt ]] ; then
-	cp /dev/null devicelog.txt
-else
-	touch devicelog.txt
-fi
-
-echo "ip address(es) of current connected devices: "
-
 active_stations=`iw wlan0 station dump | grep 'Station' | awk '{print $2}'`
 active_stations_array=($active_stations)
 
-for aS in $(cat /proc/net/arp | grep -v IP | awk '{print $4}')
-do
-	for station in ${active_stations_array[@]}
-	do
-		if [[ "$station" == "$aS" ]]; then
-			cat /proc/net/arp | grep "$station" | awk '{print $1}' | tee -a devicelog.txt
-		fi
-        done
-done
+info_() {
+  if [[ -e $/etc/nmapscripts/devicelog.txt ]] ; then
+  	cp /dev/null /etc/nmapscripts/devicelog.txt
+  else
+  	touch /etc/nmapscripts/devicelog.txt
+  fi
 
-device_times=`iw wlan0 station dump | grep 'connected' | awk '{print $3}'`
-device_times_array=($device_times)
+  echo "ip address(es) of current connected devices: "
 
-i=1
-for a in ${device_times_array[@]};
-do
-    echo "device $i has been connected for :  "
-    T=$a
-  	D=$((T/60/60/24))
-  	H=$((T/60/60%24))
-  	M=$((T/60%60))
-  	S=$((T%60))
-  	(( $D > 0 )) && printf '%d day(s) ' $D
-  	(( $H > 0 )) && printf '%d hour(s) ' $H
-  	(( $M > 0 )) && printf '%d minute(s) ' $M
-    (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
- 	 printf '%d seconds\n' $S
- 	 i=$((i+1))
-done
+  for aS in $(cat /proc/net/arp | grep -v IP | awk '{print $4}')
+  do
+  	for station in ${active_stations_array[@]}
+  	do
+  		if [[ "$station" == "$aS" ]]; then
+  			cat /proc/net/arp | grep "$station" | awk '{print $1}' | tee -a devicelog.txt
+  		fi
+          done
+  done
+
+  device_times=`iw wlan0 station dump | grep 'connected' | awk '{print $3}'`
+  device_times_array=($device_times)
+
+  i=1
+  for a in ${device_times_array[@]};
+  do
+      echo "device $i has been connected for :  "
+      T=$a
+    	D=$((T/60/60/24))
+    	H=$((T/60/60%24))
+    	M=$((T/60%60))
+    	S=$((T%60))
+    	(( $D > 0 )) && printf '%d day(s) ' $D
+    	(( $H > 0 )) && printf '%d hour(s) ' $H
+    	(( $M > 0 )) && printf '%d minute(s) ' $M
+      (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
+   	 printf '%d seconds\n' $S
+   	 i=$((i+1))
+  done
+}
+
+new_device_check() {
+  newAm=0
+
+  if [[ ! -e $/etc/nmapscripts/devicetracklog.txt ]] ; then
+    touch /etc/nmapscripts/devicetracklog.txt
+  fi
+
+  log=$(cat /etc/nmapscripts/devicetracklog.txt)
+  
+  
+  for station in ${active_stations_array[@]}
+  do
+    new=1
+
+    for line in $log
+    do
+      if [[ "$station" == "$line" ]]; then
+        new=0
+      fi
+    done
+
+    if [[ $new == 1 ]]; then
+      (( newAm++ ))
+    fi
+  done
+
+  if [[ ! $newAm == 0 ]]; then
+       /etc/init.d/emailnotification.sh start 3 $newAm
+  fi
+
+  cp /dev/null /etc/nmapscripts/devicetracklog.txt
+
+  for station in ${active_stations_array[@]}
+  do
+    echo $station | tee -a /etc/nmapscripts/devicetracklog.txt  >/dev/null
+  done
+
+  if [[ $newAm -gt 0 ]]; then
+    echo "there appears to be $newAm new device(s) on your network, please monitor. "
+  fi 
+  
+}
+
+option=$1
+case $option
+in 
+  newdevicecheck) new_device_check ;;
+  info) info_ ;;
+  *) echo "please specify an option"
+    exit ;;
+esac
 
